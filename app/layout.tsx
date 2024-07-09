825987fc8dbfff6af5507feeeba2b0d22a342cde
30in2020@gmail.com <30in2020@gmail.com>
2024-07-09 13:43:35 +0900

    ogimage

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "coldfoot",
   description: "The Hotties Cam Videos",
+  openGraph: {
+    images: `${process.env.NEXT_PUBLIC_FE_SERVER_URL}/og_image.png`,
+  },
 };
 
 export default function RootLayout({
